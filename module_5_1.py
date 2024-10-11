79471b19408616fa93f3887de6f9d537459892fe
AlekMikhTikhomirov <AlekMikh4home@gmail.com>
2024-10-11 14:38:25 +0300

    11/10/2024 "Object's attributes & methods" task is done

---
+++ module_5_1.py
@@ -0,0 +1,15 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = input("Enter the name of new house: ")
+        self.number_of_floors = int(input("Enter the number of floors: "))
+
+    def go_to(self, new_floor):
+        self.new_floor = int(input("Enter the floor number you want to go (or lift): "))
+        if self.new_floor <= self.number_of_floors:
+            for i in range(self.new_floor):
+                print(i+1)
+        elif self.new_floor > self.number_of_floors:
+            print("No such floor in this house")
+
+house_1 = House('', 0)
+house_1.go_to(0)
